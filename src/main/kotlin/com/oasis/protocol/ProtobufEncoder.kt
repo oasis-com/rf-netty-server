4408ddd05b86de1d403230ecbe73d6ac0db2ada8
mayazhou <mayazhou@oasgames.com>
2022-01-05 21:39:06 +0800

    MessageToByteEncoder change MessageToMessageEncoder

--- src/main/kotlin/com/oasis/protocol/ProtobufEncoder.kt
+++ src/main/kotlin/com/oasis/protocol/ProtobufEncoder.kt
@@ -1,14 +1,13 @@
 package com.oasis.protocol
 
 import com.oasis.protobuf.ProductReqProto
-import io.netty.buffer.ByteBuf
 import io.netty.buffer.Unpooled
 import io.netty.channel.ChannelHandlerContext
-import io.netty.handler.codec.MessageToByteEncoder
+import io.netty.handler.codec.MessageToMessageEncoder
 
-class ProtobufEncoder : MessageToByteEncoder<ProductReqProto.ProductReq>() {
+class ProtobufEncoder : MessageToMessageEncoder<ProductReqProto.ProductReq>() {
 
-    override fun encode(ctx: ChannelHandlerContext, msg: ProductReqProto.ProductReq, out: ByteBuf) {
+    override fun encode(ctx: ChannelHandlerContext, msg: ProductReqProto.ProductReq, out: MutableList<Any>) {
         // 将对象转换为 byte
         val bArray = msg.toByteArray()
 
@@ -24,6 +23,6 @@ class ProtobufEncoder : MessageToByteEncoder<ProductReqProto.ProductReq>() {
         // 消息体包含我们要发送的数据
         buf.writeBytes(bArray)
 
-        out.writeBytes(buf)
+        out.add(buf)
     }
 }
